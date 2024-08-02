6383b1dab13b5802ebcdc61a0e881509cc1216ba
Mdeba1998 <mdebashis627@gmail.com>
2024-08-02 23:17:23 +0530

    Small screen loading effect cgange

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,27 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setLoading(false);
     }, 1500);
+
+    // Cleanup the timer if the component unmounts before the timer ends
+    return () => clearTimeout(timer);
   }, []);
+
   return (
     <>
       {loading ? 
-        <div className="bg-slate-900 h-[100vh] flex justify-center items-center">
+        <div className="loading-screen">
           <BallTriangle
             height={100}
             width={100}
             radius={5}
             color="#5eead4"
             ariaLabel="ball-triangle-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
             visible={true}
           />
         </div>
